e647fbdfbd69c34655250232647a358faa90a35e
Henning Staib <github@henning-staib.de>
2019-04-05 16:43:26 +0200

    replace deprecated #should with #expect in tests

--- spec/html_attributes/array_spec.rb
+++ spec/html_attributes/array_spec.rb
@@ -4,38 +4,38 @@ describe Array do
 
   describe "#to_class_attr" do
 
-    it "should return nil if array is empty" do
+    it "returns nil if array is empty" do
       classes = []
-      classes.to_class_attr.should be_nil
+      expect(classes.to_class_attr).to be_nil
     end
 
-    it "should return a space separated string if array contains strings" do
+    it "returns a space separated string if array contains strings" do
       classes = ["first", "second", "third"]
-      classes.to_class_attr.should eql("first second third")
+      expect(classes.to_class_attr).to eql("first second third")
     end
 
-    it "should preserve existing spaces" do
+    it "preserves existing spaces" do
       classes = ["first", "second third", "forth"]
-      classes.to_class_attr.should eql("first second third forth")
+      expect(classes.to_class_attr).to eql("first second third forth")
     end
 
   end
 
   describe "#to_style_attr" do
 
-    it "should return nil if array is empty" do
+    it "returns nil if array is empty" do
       styles = []
-      styles.to_style_attr.should be_nil
+      expect(styles.to_style_attr).to be_nil
     end
 
-    it "should return a semicolon separated string if array contains strings" do
+    it "returns a semicolon separated string if array contains strings" do
       styles = ["padding-top: 8px", "padding-right: 15px", "font-size: 12pt"]
-      styles.to_style_attr.should eql("padding-top: 8px; padding-right: 15px; font-size: 12pt;")
+      expect(styles.to_style_attr).to eql("padding-top: 8px; padding-right: 15px; font-size: 12pt;")
     end
 
-    it "should preserve existing semicolon" do
+    it "preserves existing semicolon" do
       styles = ["padding-top: 8px; padding-right: 15px", "font-size: 12pt"]
-      styles.to_style_attr.should eql("padding-top: 8px; padding-right: 15px; font-size: 12pt;")
+      expect(styles.to_style_attr).to eql("padding-top: 8px; padding-right: 15px; font-size: 12pt;")
     end
 
   end
--- spec/html_attributes/hash_spec.rb
+++ spec/html_attributes/hash_spec.rb
@@ -4,84 +4,84 @@ describe Hash do
 
   describe "#to_class_attr" do
 
-    it "should return nil if hash is empty" do
+    it "returns nil if hash is empty" do
       classes = {}
-      classes.to_class_attr.should be_nil
+      expect(classes.to_class_attr).to be_nil
     end
 
-    it "should return nil if hash only contains false values" do
+    it "returns nil if hash only contains false values" do
       classes = { :foo => false, :bar => false }
-      classes.to_class_attr.should be_nil
+      expect(classes.to_class_attr).to be_nil
     end
 
-    it "should return space separated string" do
+    it "returns space separated string" do
       classes = { :first => true, :second => true, :third => true }
-      classes.to_class_attr.should eql("first second third")
+      expect(classes.to_class_attr).to eql("first second third")
     end
 
-    it "should return space separated string only for true values" do
+    it "returns space separated string only for true values" do
       classes = { :first => true, :second => false, :third => true }
-      classes.to_class_attr.should eql("first third")
+      expect(classes.to_class_attr).to eql("first third")
     end
 
   end
 
   describe "#to_style_attr" do
 
-    it "should return nil if hash is empty" do
+    it "returns nil if hash is empty" do
       styles = {}
-      styles.to_style_attr.should be_nil
+      expect(styles.to_style_attr).to be_nil
     end
 
-    it "should return nil if hash only contains nil values" do
+    it "returns nil if hash only contains nil values" do
       styles = { :display => nil }
-      styles.to_style_attr.should be_nil
+      expect(styles.to_style_attr).to be_nil
     end
 
-    it "should return semicolon separated string" do
+    it "returns semicolon separated string" do
       styles = { :display => "none", :visibility => "hidden" }
-      styles.to_style_attr.should eql("display: none; visibility: hidden;")
+      expect(styles.to_style_attr).to eql("display: none; visibility: hidden;")
     end
 
-    it "should replace '_' in keys with '-'" do
+    it "replaces '_' in keys with '-'" do
       styles = { :font_size => "12pt" }
-      styles.to_style_attr.should eql("font-size: 12pt;")
+      expect(styles.to_style_attr).to eql("font-size: 12pt;")
     end
 
-    it "should flatten hash to construct style statements" do
+    it "flattens hash to construct style statements" do
       styles = { :padding => { :top => "8px", :right => "15px" }, :font_size => "12pt" }
-      styles.to_style_attr.should eql("font-size: 12pt; padding-right: 15px; padding-top: 8px;")
+      expect(styles.to_style_attr).to eql("font-size: 12pt; padding-right: 15px; padding-top: 8px;")
     end
 
   end
 
   describe "#flatten" do
 
-    it "should do nothing for emtpy hash" do
-      {}.flatten.should eql({})
+    it "does nothing for emtpy hash" do
+      expect({}.flatten).to eql({})
     end
 
-    it "should do nothing for unnested hash" do
-      { "padding" => "8px", "font_size" => "12pt" }.flatten.should eql({ "padding" => "8px", "font_size" => "12pt" })
+    it "does nothing for unnested hash" do
+      expect({ "padding" => "8px", "font_size" => "12pt" }.flatten).to eql({ "padding" => "8px", "font_size" => "12pt" })
     end
 
-    it "should convert symbolized keys to stringified keys" do
-      { :padding => "8px", :font_size => "12pt" }.flatten.should eql({ "padding" => "8px", "font_size" => "12pt" })
+    it "converts symbolized keys to stringified keys" do
+      expect({ :padding => "8px", :font_size => "12pt" }.flatten).to eql({ "padding" => "8px", "font_size" => "12pt" })
     end
 
-    it "should flatten nested hash" do
+    it "flattens nested hash" do
       styles = { :padding => { :top => "8px", :right => "15px" } }
-      styles.flatten.should eql({ "paddingtop" => "8px", "paddingright" => "15px" })
+      expect(styles.flatten).to eql({ "paddingtop" => "8px", "paddingright" => "15px" })
     end
 
-    it "should flatten nested hash using given separator" do
+    it "flattens nested hash using given separator" do
       styles = { :padding => { :top => "8px", :right => "15px" } }
-      styles.flatten("-").should eql({ "padding-top" => "8px", "padding-right" => "15px" })
+      expect(styles.flatten("-")).to eql({ "padding-top" => "8px", "padding-right" => "15px" })
     end
 
-    it "should pass array of nested keys if block is given" do
+    it "passes array of nested keys if block is given" do
       styles = { :padding => { :top => "8px", :right => "15px" } }
-      styles.flatten { |keys| keys.join('-') }.should eql({ "padding-top" => "8px", "padding-right" => "15px" })
+      expect(styles.flatten { |keys| keys.join('-') }).to eql({ "padding-top" => "8px", "padding-right" => "15px" })
     end
 
   end
